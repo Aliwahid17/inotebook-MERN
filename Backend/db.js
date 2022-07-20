002b168cc76e99b3f9fabaccef804a4f6fde2083
wahid ali <88263575+Aliwahid17@users.noreply.github.com>
2022-07-20 12:02:57 +0530

    created schema

--- Backend/db.js
+++ Backend/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017";
+const mongoURI = 'mongodb://localhost:27017';
 
 const connectToMongo = () => {
-    mongoose.connect(mongoURI,()=>{
-        console.log("connected to mongo successfully");
+    mongoose.connect(mongoURI, () => {
+        console.log("Connected to Mongo Successfully");
     })
 }
 
